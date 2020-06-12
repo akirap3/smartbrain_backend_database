5562bbbdb85af94ecd0110f417428fe710301795
akirapf3 <akirapf3@gmail.com>
2020-06-12 08:33:36 +0800

    change db connection

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'ssssssssss973103',
-    database : 'smart_brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
